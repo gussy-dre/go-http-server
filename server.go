cb64436344ae4a27adf8ac3b429f07614a867638
ryoga-dre <ryo_gushiken_r@dreamarts.co.jp>
2023-07-19 02:11:14 +0900

    feat: 簡易的なechoサーバーを実装

--- server.go
+++ server.go
@@ -1,7 +1,58 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net"
+	"strings"
+)
+
+var (
+	ip   = flag.String("ip", "127.0.0.1", "IP Address")
+	port = flag.String("port", "8888", "Port")
+)
 
 func main() {
-	fmt.Print("Hello World!!")
+	flag.Parse()
+
+	address := fmt.Sprintf("%s:%s", *ip, *port)
+
+	listener, err := net.Listen("tcp", address)
+	if err != nil {
+		log.Fatalf("Faild to listen %s\n", address)
+	}
+	defer listener.Close()
+	log.Printf("Now listening %s\n\n", address)
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Fatalf("Failed to accept connection: %s\n", err)
+		}
+
+		buf := make([]byte, 1500)
+
+		go func() {
+			defer conn.Close()
+			log.Printf("[Remote Address] %s\n\n", conn.RemoteAddr())
+
+			reqMessage := ""
+			for {
+				n, _ := conn.Read(buf)
+				reqMessage += string(buf[:n])
+				if strings.HasSuffix(reqMessage, "\r\n\r\n") {
+					break
+				}
+			}
+			log.Printf("[Request Message]\n%s", reqMessage)
+
+			// 個々を実装する
+			resMessage := reqMessage
+
+			log.Printf("[Response Status Code] %s\n\n", "200")
+			//log.Printf("[Response Message]\n%s\n\n", resMessage)
+			conn.Write([]byte(resMessage))
+		}()
+	}
 }
