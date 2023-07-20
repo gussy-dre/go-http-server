97c0cf1f6a1a6c7c50b5aa89ddd1a8869bcbd7dd
ryoga-dre <ryo_gushiken_r@dreamarts.co.jp>
2023-07-21 01:00:15 +0900

    feat: 初期環境を準備

--- server.go
+++ server.go
@@ -1,77 +1,7 @@
 package main
 
-import (
-	"flag"
-	"fmt"
-	"log"
-	"net"
-	"strings"
-
-	"server/http"
-)
-
-var (
-	ip   = flag.String("ip", "127.0.0.1", "IP Address")
-	port = flag.String("port", "8888", "Port")
-)
+import "fmt"
 
 func main() {
-	flag.Parse()
-
-	address := fmt.Sprintf("%s:%s", *ip, *port)
-
-	listener, err := net.Listen("tcp", address)
-	if err != nil {
-		log.Fatalf("Faild to listen %s\n", address)
-	}
-	defer listener.Close()
-	fmt.Printf("Now listening %s\n\n", address)
-
-	for {
-		conn, err := listener.Accept()
-		if err != nil {
-			log.Fatalln("Failed to accept connection")
-		}
-
-		buf := make([]byte, 1500)
-
-		go func() {
-			defer conn.Close()
-			fmt.Printf("[Remote Address]\n%s\n\n", conn.RemoteAddr())
-
-			reqMessage := ""
-			for {
-				n, _ := conn.Read(buf)
-				reqMessage += string(buf[:n])
-				if strings.HasSuffix(reqMessage, "\r\n\r\n") {
-					break
-				}
-			}
-			fmt.Printf("[Request Message]\n%s", reqMessage)
-
-			req, isValid := http.CheckRequest(reqMessage)
-
-			res, contentType, err := http.ReadFile(req.Path)
-			if !isValid {
-				resMessage := http.GenerateResponse(400, "", "")
-				fmt.Printf("[Response Status Code] %s\n\n", "400")
-				//fmt.Printf("[Response Message]\n%s\n\n", resMessage)
-				conn.Write([]byte(resMessage))
-			} else if err != nil {
-				notFoundRes, contentType, err := http.ReadFile("/404.html")
-				if err != nil {
-					log.Fatal("Failed to load 404.html")
-				}
-				resMessage := http.GenerateResponse(404, contentType, notFoundRes)
-				fmt.Printf("[Response Status Code] %s\n\n", "404")
-				//fmt.Printf("[Response Message]\n%s\n\n", resMessage)
-				conn.Write([]byte(resMessage))
-			} else {
-				resMessage := http.GenerateResponse(200, contentType, res)
-				fmt.Printf("[Response Status Code] %s\n\n", "200")
-				//fmt.Printf("[Response Message]\n%s\n\n", resMessage)
-				conn.Write([]byte(resMessage))
-			}
-		}()
-	}
+	fmt.Print("Hello World!!")
 }
